be158e86ff72f017ac66a202692af18e61e714b8
jjzsilva9 <jjzsilva9@gmail.com>
2021-11-26 17:48:54 +0000

    dates and changing month added

--- icalendar/static/icalendar/calendar.js
+++ icalendar/static/icalendar/calendar.js
@@ -3,20 +3,29 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 document.addEventListener("DOMContentLoaded", function () {
     let date = new Date();
     updateMonth(date);
+
+    document.querySelector('#nextMonth').onclick = function(){
+        date.setMonth(date.getMonth()+1);
+        updateMonth(date);
+    }
+    document.querySelector('#prevMonth').onclick = function(){
+        date.setMonth(date.getMonth()-1);
+        updateMonth(date);
+    }
 })
 
 function updateMonth(date){
     let month = date.getUTCMonth();
-    document.querySelector('#thisMonth').innerText = months[month];
+    document.querySelector('#thisMonth').innerText = months[month] + ' ' + date.getFullYear();
     var firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
-    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
     
-    var lastDayPrev = new Date(date.getFullYear(), date.getMonth(), 0).getDay();
-    console.log(firstDay);
+    var lastDayPrev = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
     document.querySelectorAll('.day').forEach(day => {
-        day.innerText -= firstDay;
+        day.innerText = day.id - firstDay;
+        day.style.color = "black"
         if (day.innerText < 1){
-            day.innerText = lastDayPrev - day.innerText;
+            day.innerText =  Number(lastDayPrev) + Number(day.innerText);
             day.style.color = "lightgrey";
         } else if (day.innerText > lastDay){
             day.innerText = day.innerText - lastDay;
