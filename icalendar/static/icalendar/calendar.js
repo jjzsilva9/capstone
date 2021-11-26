27556a4d93f60a6403b4449211f2e772feecaef9
jjzsilva9 <73418407+jjzsilva9@users.noreply.github.com>
2021-11-26 12:21:16 +0000

    finished day changes
    changes numbers to reflect the dates of the month

--- icalendar/static/icalendar/calendar.js
+++ icalendar/static/icalendar/calendar.js
@@ -9,8 +9,18 @@ function updateMonth(date){
     let month = date.getUTCMonth();
     document.querySelector('#thisMonth').innerText = months[month];
     var firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+    
+    var lastDayPrev = new Date(date.getFullYear(), date.getMonth(), 0).getDay();
     console.log(firstDay);
     document.querySelectorAll('.day').forEach(day => {
         day.innerText -= firstDay;
+        if (day.innerText < 1){
+            day.innerText = lastDayPrev - day.innerText;
+            day.style.color = "lightgrey";
+        } else if (day.innerText > lastDay){
+            day.innerText = day.innerText - lastDay;
+            day.style.color = "lightgrey";
+        }
     });
 }
